69dfffde6cbbb4d6221f2e3bf3c89ce3c0690bb3
Samuel Tawiah <Samuelekowtawiah2@gmail.com>
2026-08-30 18:47:23 +0000

    refactor: add managed database session dependency

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 DATABASE_URL = "sqlite:///./gym.db"
 
@@ -9,7 +9,15 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
     bind=engine,
+    autoflush=False,
+    autocommit=False,
 )
+
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
